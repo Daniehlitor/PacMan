81ea1374b90796ac8fbc16f54beef44f0e1a7d4e
Daniel Jerez <daniehlitor@gmail.com>
2025-04-30 19:39:26 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector(".grid");
+    const cells = Array.from(grid.querySelector('div'));
+
+    const width = 10;
+    let pacManIndex = 11;
+
+    class Ghost {
+        constructor(name, startIndex, className, speed = 500) {
+            this.name = name;
+            this.currentIndex = startIndex;
+            this.className = className;
+            this.speed = speed;
+            this.timerId = null;
+            this.directions = [-1, 1, -width, width]
+        }
+    }
+
+    const blinky = new Ghost('blinky', 35, 'red', 500);
+    const pinky = new Ghost('pinky', 36, 'pink', 600);
+    const ghosts = [blinky, pinky];
+});
